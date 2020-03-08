f592754fed77465e6941328c9208b049bc04cb13
daichan0428 <daichan0428@hotmail.com>
2020-03-08 11:14:07 +0900

    add 出品商品一覧

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,11 @@
 class ItemsController < ApplicationController
   def show
-  @item = Item.find(params[:id])
+    @item = Item.find(params[:id])
   end
+
+  def index
+    @items = Item.all
+  end
+
+
 end
